c1b6a2a3a0e291e5bf51a4aad125fbb0d4fd8d61
AIMARD <be.aimard@gmail.com>
2015-01-09 14:30:45 +0100

    Modification ConnexionGoogle

--- TriePhotoV2/src/main/java/juxo/apiCalendar/connexionGoogle/OAuth2Token.java
+++ TriePhotoV2/src/main/java/juxo/apiCalendar/connexionGoogle/OAuth2Token.java
@@ -1,7 +1,5 @@
 package juxo.apiCalendar.connexionGoogle;
 
-import javax.ws.rs.client.Client;
-
 import org.glassfish.jersey.client.oauth2.ClientIdentifier;
 import org.glassfish.jersey.client.oauth2.OAuth2ClientSupport;
 import org.glassfish.jersey.client.oauth2.OAuth2CodeGrantFlow;
@@ -11,32 +9,26 @@ import org.glassfish.jersey.client.oauth2.TokenResult;
 
 public class OAuth2Token {
 	
-    private OAuth2CodeGrantFlow flow;
-    private String authURI;
-    private String token;
-    private String refreshToken;
-    private String type; 
-    private int expirationDelay;
-    
-    /***
-     * Si on a déjà une clef d'accès
-     * @param token
-     */
-    public OAuth2Token(String token) {
-    	this.token = token;
-    }
+    public OAuth2CodeGrantFlow flow;
+    public String authURI;
+    public String tokenAcess;
+    public String refreshToken;
+    public String type; 
+    public int expirationDelay;
+	public static String OOB = "urn:ietf:wg:oauth:2.0:oob";
+	
     /***
      * Construction d'un objet token selon les spécifications google
      * @param clientId
      * @param callbackURI
      * @param scope
      */
-    public OAuth2Token(ClientIdentifier clientId, String callbackURI, String scope) {
-    	Builder<OAuth2FlowGoogleBuilder> builder = OAuth2ClientSupport.googleFlowBuilder(clientId, callbackURI, scope);
-
+    public OAuth2Token(ClientIdentifier clientId, String scope) {
+    	Builder<OAuth2FlowGoogleBuilder> builder = 
+    			OAuth2ClientSupport.googleFlowBuilder(clientId, OOB, scope);
 		flow = builder
 				.property(OAuth2CodeGrantFlow.Phase.AUTHORIZATION, "readOnly","true")
-				.property(OAuth2CodeGrantFlow.Phase.AUTHORIZATION,"redirect_uri", callbackURI)
+				.property(OAuth2CodeGrantFlow.Phase.AUTHORIZATION,"redirect_uri", OOB)
 				.property(OAuth2CodeGrantFlow.Phase.AUTHORIZATION, "state", "")
 				.scope(scope).build();
     
@@ -44,44 +36,24 @@ public class OAuth2Token {
     }
     
     /***
+     * Si on a déjà une clef d'accès
+     * @param token
+     */
+    public OAuth2Token(String token) {
+    	this.tokenAcess = token;
+    }
+    
+    /***
      * On récupère l'accès après avoir fournit le code
      * @param code
      */
     public void doAccessTokenRequest(String code) {
+    	
     	TokenResult result = flow.finish(code, ""); // we do not provide state
-        
-        this.token = result.getAccessToken();
+        this.tokenAcess = result.getAccessToken();
         this.refreshToken = (String) result.getAllProperties().get("refresh_token");
         this.expirationDelay = (int) result.getAllProperties().get("expires_in");
         this.type = (String) result.getAllProperties().get("token_type");
     }
 
-    //Plein d'accesseur
- 
-    public String getToken() {
-    	return token;
-    }
-
-    public String getTokenType() {
-    	return type;
-    }
-
-    public int getExpiration() {
-    	return expirationDelay;
-    }
-    
-    public String getRefreshToken() {
-    	return refreshToken;
-    }
-    
-    public Client getOAuth2Client() {
-        return flow.getAuthorizedClient();
-    }
-    public OAuth2CodeGrantFlow getFlow() {
-    	return flow;
-    }
-    
-    public String getValidateURI() {
-    	return authURI;
-    }
 }
--- TriePhotoV2/src/test/java/juxo/triephotoV2/apiCalendar/apiCalendarTest.java
+++ TriePhotoV2/src/test/java/juxo/triephotoV2/apiCalendar/apiCalendarTest.java
@@ -1,5 +1,22 @@
 package juxo.triephotoV2.apiCalendar;
 
-public class apiCalendarTest {
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import junit.framework.TestCase;
+import juxo.apiCalendar.connexionGoogle.ConnexionGoogle;
+
+
+
+
+public class apiCalendarTest extends TestCase {
+
+	private final String API_KEY = "125768752842-8kgilik6k7ucmbhph49kqoia3bum3pqr.apps.googleusercontent.com";
+	private final String API_SECRET ="oKcIMqOmQKDIuT8_Xhw9SKBE";
+	
+	public void testConnexionGoogle() throws IOException, URISyntaxException{
+		ConnexionGoogle c = new ConnexionGoogle(API_KEY, API_SECRET);
+		assert c.token.tokenAcess!=null;
+	}
+	
 }
