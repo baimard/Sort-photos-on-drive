de172095dead4612483c76716db8d3b3e2f493da
Grazilena <rayivirelena1@gmail.com>
2015-01-14 11:22:35 +0100

    Augusta mise à jour des fichiers ignorés

---
+++ TriePhotoV2/src/main/java/juxo/notification/NotificationListener.java
@@ -0,0 +1,5 @@
+package juxo.notification;
+
+public class NotificationListener {
+
+}
---
+++ TriePhotoV2/src/test/java/juxo/triephotoV2/NotificationTest.java
@@ -0,0 +1,15 @@
+package juxo.triephotoV2;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+import juxo.notification.AfficherNotification;
+
+public class NotificationTest extends TestCase {
+	
+	public void testNotif() throws IOException{
+		AfficherNotification testnotification =  new AfficherNotification();
+		testnotification.notification();
+	}
+	//test
+}
