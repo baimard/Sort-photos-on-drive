b389e86d2e510c558c154db95276247132757c9d
Grazilena <rayivirelena1@gmail.com>
2015-01-14 11:04:36 +0100

    Notification qui marche Augusta

--- TriePhotoV2/src/main/java/juxo/notification/AfficherNotification.java
+++ TriePhotoV2/src/main/java/juxo/notification/AfficherNotification.java
@@ -3,9 +3,13 @@ package juxo.notification;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 public class AfficherNotification {
 	
-		public void notification() {
+		public void notification() throws IOException {
 						
 			// On verifie que le support de la figure du SystemTray est compatible avec l'OS hot
 			if (SystemTray.isSupported()) {
@@ -15,7 +19,10 @@ public class AfficherNotification {
 			    SystemTray tray = SystemTray.getSystemTray();
 			    
 			    // On recupere l'image qui nous servira d'icone
-	            Image image = Toolkit.getDefaultToolkit().getImage("triephoto.gif"); 
+	          //  Image image = Toolkit.getDefaultToolkit().getImage("triephoto.gif"); 
+			    BufferedImage imageBuffered= ImageIO.read(getClass().getResource("triephoto.gif"));
+			    int trayIconWidth= new TrayIcon(imageBuffered).getSize().width;
+			    final TrayIcon trayIcon1 = new TrayIcon(imageBuffered.getScaledInstance(trayIconWidth, -1, Image.SCALE_SMOOTH));
 	            // Notre menu (clic droit sur l'icone systray)
 			    PopupMenu popup = new PopupMenu();
 			    MenuItem defaultItem = new MenuItem("Quitter");
@@ -28,19 +35,19 @@ public class AfficherNotification {
 			    	
 			    popup.add(defaultItem);
 			    // Creation de l'icone systray
-			    trayIcon = new TrayIcon(image, "Trie Photo", popup); 
+			    //trayIcon = new TrayIcon(image, "Trie Photo", popup); 
 
 			    		ActionListener actionListener = new ActionListener() {
 			    		public void actionPerformed(ActionEvent e) {
-			    			trayIcon.displayMessage("Trie Photo disponible",
+			    			trayIcon1.displayMessage("Trie Photo disponible",
 			             null, TrayIcon.MessageType.INFO);
 			    		}
 			    		};
-			    trayIcon.setImageAutoSize(true);
-			    trayIcon.addActionListener(actionListener);
+			    trayIcon1.setImageAutoSize(true);
+			    trayIcon1.addActionListener(actionListener);
 			    
 				    	try {
-				    		tray.add(trayIcon);} 
+				    		tray.add(trayIcon1);} 
 				    	catch (AWTException e) {
 				        e.printStackTrace();
 				    	}
@@ -48,6 +55,6 @@ public class AfficherNotification {
 			} else {
 				// ...
 			}
-		
+		System.out.println("Stop Debug");
 	}
 }
