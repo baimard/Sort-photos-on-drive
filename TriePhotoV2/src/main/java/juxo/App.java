3055f8f863394df5844221f93f604d7007648f37
rianaTDA <rianar2013@gmail.com>
2015-01-20 16:55:47 +0100

    Riana
    Commit

--- TriePhotoV2/src/main/java/juxo/App.java
+++ TriePhotoV2/src/main/java/juxo/App.java
@@ -2,13 +2,15 @@ package juxo;
 
 import java.awt.AWTException;
 import java.io.IOException;
+
+import juxo.UiTriePhotoV2.UiUser;
 import juxo.notification.AfficherNotification;
 
 public class App 
 {
 	public static void main( String[] args ) throws IOException
     {
-		new UiUser();
+		//new UiUser();
 		try {
 			new AfficherNotification();
 		} catch (AWTException e) {
