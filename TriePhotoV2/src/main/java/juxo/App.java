c9534ac17746f9e28ad099810390eb8ab7543f20
AIMARD <be.aimard@gmail.com>
2015-01-20 14:31:13 +0100

    Benjamin aimard
    Modification de nom de package + Classe

--- TriePhotoV2/src/main/java/juxo/App.java
+++ TriePhotoV2/src/main/java/juxo/App.java
@@ -2,17 +2,19 @@ package juxo;
 
 import java.awt.AWTException;
 import java.io.IOException;
+
+import juxo.UiTriePhotoV2.UiUser;
 import juxo.notification.AfficherNotification;
 
 public class App 
 {
 	public static void main( String[] args ) throws IOException
     {
-		//new UiUser();
-		try {
+		new UiUser();
+		/*try {
 			new AfficherNotification();
 		} catch (AWTException e) {
 			e.printStackTrace();
-		}
+		}*/
     }
 }
