db0e216eaf37b6347358ca78c10e850a126b62b7
AIMARD <be.aimard@gmail.com>
2015-01-20 14:51:24 +0100

    Création d'une classe de paramètre

---
+++ TriePhotoV2/src/test/java/juxo/parametrage/parametrageTest.java
@@ -0,0 +1,26 @@
+package juxo.parametrage;
+
+import juxo.system.Parametrage;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class parametrageTest {
+	
+	@Test
+	public void testInstanciationParametrage(){
+		new Parametrage("/Users/Juxo", "/Users/Juxo");
+	}
+	
+	@Test
+	public void testSerialisationParametrage(){
+		Parametrage.getInstance().enregistrerObjet();
+	}
+	
+	@Test
+	public void testChargementSerialisationParametrage(){
+		Parametrage.chargerObjet();
+		Parametrage p = Parametrage.getInstance();
+		System.out.println("Dossier source : " + p.getDossierSource() + " Dossier Destination : " + p.getDossierDestination());
+	}
+}
