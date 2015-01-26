580009874353fc0aba1dafd91fdb5726ae1a4ec8
rianaTDA <rianar2013@gmail.com>
2015-01-26 16:20:27 +0100

    ok

---
+++ TriePhotoV2/src/test/java/juxo/ui/UiTest.java
@@ -0,0 +1,19 @@
+package juxo.ui;
+
+import javax.swing.JFrame;
+
+import juxo.UiTriePhotoV2.UiQuestion;
+import juxo.apiCalendar.Evenements;
+import juxo.triephotoV2.accessFichier.Fichiers;
+
+import org.junit.Test;
+
+public class UiTest {
+	@Test
+ public void testUiQuestion(){
+		
+		
+	//ew UiQuestion(JFrame parent, String title, boolean modal, Evenements l, Fichiers fs);
+	 
+ }
+}
