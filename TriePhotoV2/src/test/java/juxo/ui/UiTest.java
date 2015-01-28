da22838db009530765ff0c29762d18392d58b407
rianaTDA <rianar2013@gmail.com>
2015-01-28 09:23:39 +0100

    Riana
    commit

--- TriePhotoV2/src/test/java/juxo/ui/UiTest.java
+++ TriePhotoV2/src/test/java/juxo/ui/UiTest.java
@@ -1,27 +1,49 @@
 package juxo.ui;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.swing.JFrame;
 
 import juxo.UiTriePhotoV2.UiQuestion;
+import juxo.apiCalendar.Evenement;
 import juxo.apiCalendar.Evenements;
+import juxo.triephotoV2.accessFichier.Fichier;
 import juxo.triephotoV2.accessFichier.Fichiers;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class UiTest {
-	@Test
 	
-	public void UiParametre() {
+	@Ignore
+	@Test
+    public void testUiQuestion() throws InterruptedException {
 		
+		Evenement e1 = new Evenement("testEv", new Date(1,1,2015), new Date(2,1,2015));
+		Evenements evenements = new Evenements();
+		evenements.add(e1);
+		
+		Fichier f1 = new Fichier("C:\\Users\\RAKOTOMALALA\\Documents\\Projet Image Test\\1.jpg");
+		Fichier f2 = new Fichier("C:\\Users\\RAKOTOMALALA\\Documents\\Projet Image Test\\2.jpg");
+		Fichier f3 = new Fichier("C:\\Users\\RAKOTOMALALA\\Documents\\Projet Image Test\\3.jpg");
+		Fichiers fichiers = new Fichiers();
+		fichiers.add(f1);
+		fichiers.add(f2);
+		fichiers.add(f3);
+		
+		UiQuestion quest = new UiQuestion(null, "Test", true, evenements, fichiers);
+		quest.showUiQuestion();
+		//Thread.sleep(5000);
 	}
 	
 	@Test
-    public void testUiQuestion(JFrame parent, String title, boolean modal, Evenements l, Fichiers fs) {
-		
-	 
- }
+	public void testUiParametre() throws InterruptedException  {
+		UiParametre param = new UiParametre();
+		Thread.sleep(5000);
+	}
+	
+	
     
     
 }
