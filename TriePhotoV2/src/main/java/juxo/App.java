e15cfdb72cd7182c8f1b960e851e96724899df5a
AIMARD <be.aimard@gmail.com>
2015-02-10 15:10:38 +0100

    Synchro

--- TriePhotoV2/src/main/java/juxo/App.java
+++ TriePhotoV2/src/main/java/juxo/App.java
@@ -1,27 +1,16 @@
 ﻿package juxo;
 
 import java.awt.AWTException;
-import java.awt.Image;
-import java.awt.Toolkit;
 import java.io.IOException;
 
 import juxo.UiTriePhotoV2.UiPremierDemarrage;
 import juxo.notification.AfficherNotification;
 import juxo.system.Parametrage;
 import juxo.threads.ProcessObservationDossier;
-import juxo.triephotoV2.accessFichier.Fichier;
 
-import com.apple.eawt.Application;
-
-@SuppressWarnings("restriction")
 public class App {
 	public static void main(String[] args) throws IOException {
-		Application application = Application.getApplication();
-		Image icone = Toolkit.getDefaultToolkit().getImage(
-				UiPremierDemarrage.class.getResource("Resource"
-						+ Fichier.SEPARATOR + "icone-pioupiou.jpg"));
-		application.setDockIconImage(icone);
-
+		
 		try {
 			new AfficherNotification();
 			AfficherNotification.AfficherMsgNotification("Application Lancée");
