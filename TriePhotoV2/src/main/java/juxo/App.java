016fbb1ab5b1772b75dd0ed55fb137fdbd98d840
AIMARD <be.aimard@gmail.com>
2015-02-10 15:08:44 +0100

    Syncrho

--- TriePhotoV2/src/main/java/juxo/App.java
+++ TriePhotoV2/src/main/java/juxo/App.java
@@ -1,35 +1,42 @@
 ﻿package juxo;
 
 import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.io.IOException;
 
 import juxo.UiTriePhotoV2.UiPremierDemarrage;
-import juxo.UiTriePhotoV2.UiUser;
-import juxo.apiCalendar.connexionGoogle.ConnexionGoogle;
 import juxo.notification.AfficherNotification;
 import juxo.system.Parametrage;
 import juxo.threads.ProcessObservationDossier;
+import juxo.triephotoV2.accessFichier.Fichier;
 
-public class App 
-{
-	public static void main( String[] args ) throws IOException
-    {
+import com.apple.eawt.Application;
 
-	try {
+@SuppressWarnings("restriction")
+public class App {
+	public static void main(String[] args) throws IOException {
+		Application application = Application.getApplication();
+		Image icone = Toolkit.getDefaultToolkit().getImage(
+				UiPremierDemarrage.class.getResource("Resource"
+						+ Fichier.SEPARATOR + "icone-pioupiou.jpg"));
+		application.setDockIconImage(icone);
+
+		try {
 			new AfficherNotification();
 			AfficherNotification.AfficherMsgNotification("Application Lancée");
 			Parametrage.chargerObjet();
-			
-			if(Parametrage.getInstance() == null){
-				new Parametrage(null,null);
+
+			if (Parametrage.getInstance() == null) {
+				new Parametrage(null, null);
 				new UiPremierDemarrage();
-			}else{
+			} else {
 				Thread t = new ProcessObservationDossier();
 				t.start();
 			}
-			
+
 		} catch (AWTException e) {
 			e.printStackTrace();
 		}
-    }
+	}
 }
